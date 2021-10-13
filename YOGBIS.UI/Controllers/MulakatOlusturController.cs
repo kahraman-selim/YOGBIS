26262ede45156fbcfe63988fa395b5a55a4621e8
Selim KAHRAMAN <74318957+kahraman-selim@users.noreply.github.com>
2021-10-13 13:40:26 +0300

    ikinciyuklemeoncesi

---
+++ YOGBIS.UI/Controllers/MulakatOlusturController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YOGBIS.UI.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class MulakatOlusturController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ YOGBIS.UI/Controllers/OgretmenlerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YOGBIS.UI.Controllers
+{
+    public class OgretmenlerController : Controller
+    {
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
