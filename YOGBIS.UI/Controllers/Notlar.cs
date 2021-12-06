127b149f9225c18626b2f29961f1f2ef9da1bd02
selim kahraman <kahramanselim@hotmail.com>
2021-12-06 16:48:29 +0300

    calender eklendi

---
+++ YOGBIS.UI/Controllers/Notlar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YOGBIS.UI.Controllers
+{
+    public class Notlar : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
