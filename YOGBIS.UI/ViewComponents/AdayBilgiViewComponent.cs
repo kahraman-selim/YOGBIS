99ee3723b0f29f861f484a0d1d0110f5c8e384cc
selim kahraman <kahramanselim@hotmail.com>
2025-01-15 21:08:38 +0300

    yapılan son değişiklikler

---
+++ YOGBIS.UI/ViewComponents/AdayBilgiViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using YOGBIS.BusinessEngine.Contracts;
+
+namespace YOGBIS.UI.ViewComponents
+{
+    public class AdayBilgiViewComponent : ViewComponent
+    {
+
+        private readonly IAdaylarBE _adaylarBE;
+
+        public AdayBilgiViewComponent(IAdaylarBE adaylarBE)
+        {
+            _adaylarBE = adaylarBE;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var requestmodel = _adaylarBE.AdaylariGetir();
+           // if (requestmodel.IsSuccess)
+           // {
+           //     return View(requestmodel.Data);
+          //  }
+            return View();
+        }
+    }
+}
---
+++ YOGBIS.UI/ViewComponents/AdayNotViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using YOGBIS.BusinessEngine.Contracts;
+
+namespace YOGBIS.UI.ViewComponents
+{
+    public class AdayNotViewComponent : ViewComponent
+    {
+        private readonly IAdaylarBE _adaylarBE;
+
+        public AdayNotViewComponent(IAdaylarBE adaylarBE)
+        {
+            _adaylarBE = adaylarBE;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
