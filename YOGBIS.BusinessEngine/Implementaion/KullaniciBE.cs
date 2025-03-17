9e366b9977ef71c35602d48af2da14abda151d50
selim kahraman <kahramanselim@hotmail.com>
2025-03-17 23:06:58 +0300

    kullanıcı düzenleme

--- YOGBIS.BusinessEngine/Implementaion/KullaniciBE.cs
+++ YOGBIS.BusinessEngine/Implementaion/KullaniciBE.cs
@@ -36,7 +36,7 @@ namespace YOGBIS.BusinessEngine.Implementaion
         #region KullaniciGetir
         public Result<List<KullaniciVM>> KullaniciGetir()
         {
-            var data = _unitOfWork.kullaniciRepository.GetAll().OrderBy(k=>k.TcKimlikNo).ToList();
+            var data = _unitOfWork.kullaniciRepository.GetAll().OrderBy(t=>t.TcKimlikNo).ToList();
             var kullanicilar = _mapper.Map<List<Kullanici>, List<KullaniciVM>>(data);
             return new Result<List<KullaniciVM>>(true, ResultConstant.RecordFound, kullanicilar);
         }
