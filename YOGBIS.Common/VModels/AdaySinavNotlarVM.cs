f131edef9232fc60142ba9968742c73728a8bcf0
selim kahraman <kahramanselim@hotmail.com>
2025-03-21 04:17:06 +0300

    mülakat sayfa düzeltildi.

--- YOGBIS.Common/VModels/AdaySinavNotlarVM.cs
+++ YOGBIS.Common/VModels/AdaySinavNotlarVM.cs
@@ -5,83 +5,72 @@ namespace YOGBIS.Common.VModels
 {
     public class AdaySinavNotlarVM : BaseVM
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; }        
 
-        [Required(ErrorMessage = "TC Kimlik No zorunludur")]
-        [StringLength(11, MinimumLength = 11, ErrorMessage = "TC Kimlik No 11 haneli olmalıdır")]
-        [Display(Name = "TC Kimlik No")]
-        public string TC { get; set; }
-
-        public Guid? KomisyonId { get; set; }
-
-        [Display(Name = "Komisyon")]
-        public KomisyonlarVM Komisyon { get; set; }
-
-        [Display(Name = "Komisyon Üye Sıra No")]
+        public Guid KomisyonId { get; set; }
         public int KomisyonUyeSiraId { get; set; }
-
-        public Guid? NotKategoriId1 { get; set; }
-
-        [Display(Name = "Not Kategorisi 1")]
-        public SoruKategorilerVM NotKategorisi1 { get; set; }
-
-        [Range(0, 100, ErrorMessage = "Not 0-100 arasında olmalıdır")]
-        [Display(Name = "Not 1")]
+        public string KomisyonAdi { get; set; }
+        public string KomisyonUyeAdSoyad { get; set; }
+        [Display(Name = "Komisyon")]
+        public KomisyonlarVM Komisyon { get; set; }  
+      
+        public Guid NotKategoriId1 { get; set; }
+        public string KategoriAdi1 { get; set; }
+        public SoruKategorilerVM NotKategori1 { get; set; }
         public int Not1 { get; set; }
 
-        public Guid? NotKategoriId2 { get; set; }
-
-        [Display(Name = "Not Kategorisi 2")]
-        public SoruKategorilerVM NotKategorisi2 { get; set; }
-
-        [Range(0, 100, ErrorMessage = "Not 0-100 arasında olmalıdır")]
-        [Display(Name = "Not 2")]
+        public Guid NotKategoriId2 { get; set; }
+        public string KategoriAdi2 { get; set; }
+        public SoruKategorilerVM NotKategori2 { get; set; }
         public int Not2 { get; set; }
 
-        public Guid? NotKategoriId3 { get; set; }
-
-        [Display(Name = "Not Kategorisi 3")]
-        public SoruKategorilerVM NotKategorisi3 { get; set; }
-
-        [Range(0, 100, ErrorMessage = "Not 0-100 arasında olmalıdır")]
-        [Display(Name = "Not 3")]
+        public Guid NotKategoriId3 { get; set; }
+        public string KategoriAdi3 { get; set; }
+        public SoruKategorilerVM NotKategori3 { get; set; }
         public int Not3 { get; set; }
 
-        public Guid? NotKategoriId4 { get; set; }
-
-        [Display(Name = "Not Kategorisi 4")]
+        public Guid NotKategoriId4 { get; set; }
+        public string KategoriAdi4 { get; set; }
         public SoruKategorilerVM NotKategorisi4 { get; set; }
-
-        [Range(0, 100, ErrorMessage = "Not 0-100 arasında olmalıdır")]
-        [Display(Name = "Not 4")]
         public int Not4 { get; set; }
 
-        public Guid? NotKategoriId5 { get; set; }
-
-        [Display(Name = "Not Kategorisi 5")]
+        public Guid NotKategoriId5 { get; set; }
+        public string KategoriAdi5 { get; set; }
         public SoruKategorilerVM NotKategorisi5 { get; set; }
-
-        [Range(0, 100, ErrorMessage = "Not 0-100 arasında olmalıdır")]
-        [Display(Name = "Not 5")]
         public int Not5 { get; set; }
 
-        [Required(ErrorMessage = "Aday seçimi zorunludur")]
-        public Guid AdayId { get; set; }
+        public int Toplam1 { get; set; }
+        public int Toplam2 { get; set; }
+        public int Toplam3 { get; set; }
+        public double Ortalama { get; set; }
+        public double MYSPuan { get; set; }
+        public string MYSSonuc { get; set; }
 
+        public Guid AdayId { get; set; }
+        public string TC { get; set; }
+        public string AdayAdSoyad { get; set; }
+        public string AdayMYYSPuan { get; set; }
         [Display(Name = "Aday")]
         public AdaylarVM Adaylar { get; set; }
 
+        public Guid BransId { get; set; }
+        public string BransAdi { get; set; }
+        [Display(Name = "Branş")]
+        public BranslarVM Branslar { get; set; }
+
+        public Guid UlkeTercihId { get; set; }
+        public string UlkeAdi { get; set; }
+        [Display(Name = "Ülke Tercih")]
+        public UlkeTercihVM UlkeTercihler { get; set; }
+
         public Guid? MulakatId { get; set; }
-
         [Display(Name = "Mülakat")]
-        public MulakatlarVM Mulakat { get; set; }
+        public MulakatlarVM Mulakatlar { get; set; }
 
-        [Required(ErrorMessage = "Kaydeden kişi zorunludur")]
+       
         public string KaydedenId { get; set; }
-
         [Display(Name = "Kaydeden")]
         public string KaydedenAdi { get; set; }
-
         public KullaniciVM Kullanici { get; set; }
     }
 }
